7117af518f888b5e8c1a2658c2268e9ebe0156f4
xandr <moroz070688@gmail.com>
2024-07-02 14:27:19 +0300

    swagger_added

--- internal/routers/movieRouter.go
+++ internal/routers/movieRouter.go
@@ -1,7 +1,10 @@
 package routers
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
+	httpSwagger "github.com/swaggo/http-swagger/v2"
 
 	"github.com/XanderMoroz/mongoMovies/internal/controllers"
 )
@@ -10,10 +13,21 @@ import (
 func Router() *mux.Router {
 
 	router := mux.NewRouter()
+
+	// Swagger routes
+	router.PathPrefix("/swagger/").Handler(httpSwagger.Handler(
+		//The url pointing to API definition
+		// httpSwagger.URL("http://localhost:4000/swagger/docs.json"),
+		httpSwagger.DeepLinking(true),
+		httpSwagger.DocExpansion("none"),
+		httpSwagger.DomID("swagger-ui"),
+	)).Methods(http.MethodGet)
+
 	router.HandleFunc("/api/movies", controllers.GetAlIMovies).Methods("GET")
 	router.HandleFunc("/api/movie", controllers.CreateMovie).Methods("POST")
 	router.HandleFunc("/api/movie/{id}", controllers.MarkAsWatched).Methods("PUT")
 	router.HandleFunc("/api/movie/{id}", controllers.DeleteMovie).Methods("DELETE")
-	router.HandleFunc("/api/delallmovies", controllers.DeleteAllMovies).Methods("DELETE")
+	router.HandleFunc("/api/deleteallmovies", controllers.DeleteAllMovies).Methods("DELETE")
+
 	return router
 }
